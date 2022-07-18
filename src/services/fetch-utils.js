4b514f02739927b4232187625ef093b5066f91ff
Chad Stabler <stablerpsn@gmail.com>
2022-07-18 12:59:24 -0500

    fixing one small error with quotes

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,4 +1,4 @@
-import { client } from "./client";
+import { client } from './client';
 
 export async function signUp(email, password) {
   const { user } = await client.from('users').signUp(email, password);

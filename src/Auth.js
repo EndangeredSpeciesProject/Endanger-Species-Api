3e085329ad23300258fd9841d945880977fb9587
austinbhan <austinbhan@outlook.com>
2022-07-19 16:41:20 -0700

    Title and mission statement on auth

--- src/Auth.js
+++ src/Auth.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { signIn, signUp } from './services/fetch-utils';
+import './App.css';
 //import { useDataContext } from './DataProvider';
 
 export default function AuthPage() {
@@ -38,39 +39,45 @@ export default function AuthPage() {
   }
 
   return (
-    <div className="Auth">
-      <form onSubmit={handleSignUp} className="signUp">
-        <label>
-          <h2>Sign Up</h2> 
+    <>
+      <div className="Auth">
+        <div className="auth-header">
+          <h1>Title</h1>
+          <h3>Mission Statement</h3>
+        </div>
+        <form onSubmit={handleSignUp} className="signUp">
+          <label>
+            <h2>Sign Up</h2> 
           E-mail:
-          <input value={signUpEmail} onChange={(e) => setSignUpEmail(e.target.value)} />
-        </label>
-        <label>
+            <input value={signUpEmail} onChange={(e) => setSignUpEmail(e.target.value)} />
+          </label>
+          <label>
           Password:
-          <input
-            value={signUpPass}
-            type="password"
-            onChange={(e) => setSignUpPass(e.target.value)}
-          />
-        </label>
-        <button className="button">sign up</button>
-      </form>
-      <form onSubmit={handleSignIn} className="signIn">
-        <label>
-          <h2>Sign In</h2>
+            <input
+              value={signUpPass}
+              type="password"
+              onChange={(e) => setSignUpPass(e.target.value)}
+            />
+          </label>
+          <button className="button">sign up</button>
+        </form>
+        <form onSubmit={handleSignIn} className="signIn">
+          <label>
+            <h2>Sign In</h2>
           E-mail:
-          <input value={signInEmail} onChange={(e) => setSignInEmail(e.target.value)} />
-        </label>
-        <label>
+            <input value={signInEmail} onChange={(e) => setSignInEmail(e.target.value)} />
+          </label>
+          <label>
           Password:
-          <input
-            value={signInPass}
-            type="password"
-            onChange={(e) => setSignInPass(e.target.value)}
-          />
-        </label>
-        <button className="button">sign in</button>
-      </form>
-    </div>
+            <input
+              value={signInPass}
+              type="password"
+              onChange={(e) => setSignInPass(e.target.value)}
+            />
+          </label>
+          <button className="button">sign in</button>
+        </form>
+      </div>
+    </>
   );
 }

fc009be2e169b6b4127e2e7d7c1ac005fd816db6
Pedro <pedro.chn01@gmail.com>
2022-07-19 15:23:00 -0700

    fixing ci

--- src/DetailFish.js
+++ src/DetailFish.js
@@ -32,18 +32,6 @@ export default function DetailFish() {
       <h1>{fish['Species Name']}</h1>
       <h2>{fish['Scientific Name']}</h2>
       <img className="fish-pic" src={fish['Species Illustration Photo'].src}/>
-<<<<<<< HEAD
-      <div className='bio'>Biology: {<MyComponent prop={fish.Biology}/>}</div>
-      <div className='habitat'>Habitat: {<MyComponent prop={fish.Habitat}/>}</div>
-      <div>Nutritional facts: 
-        <p>Serving Weight: {fish['Serving Weight']}</p>
-        <p>Total fats: {fish['Fat, Total']}</p>
-        <p>Protein: {fish.Protein}</p>
-        <p>Cholesterol: {fish['Cholesterol']}</p>
-        <p>Sodium: {fish.Sodium}</p>
-        <MyComponent className='taste' prop={fish.Taste}/>
-        <MyComponent className='texture' prop={fish.Texture}/>
-=======
       <div>Biology: {<MyComponent prop={fish.Biology}/>}</div>
       <div>Habitat: {<MyComponent prop={fish.Habitat}/>}</div>
       <div>

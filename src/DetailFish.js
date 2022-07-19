befc9580e509344f2fb08c8015fa83072c18c89b
Chad Stabler <stablerpsn@gmail.com>
2022-07-18 19:34:37 -0500

    dangerously set inner html working for data munging

--- src/DetailFish.js
+++ src/DetailFish.js
@@ -5,20 +5,31 @@ import { fetchUnoFish } from './services/fetch-utils';
 import React from 'react';
 
 export default function DetailFish() {
+
   const params = useParams();
   const [fish, setfish] = useState({});
     
   useEffect(() => {
     async function fetchSingleFish(name) {
       const data = await fetchUnoFish(name);
+      console.log(data[0]);
       setfish(data[0]);
     }
     fetchSingleFish(params.name);
   }, []); 
+  function createMarkup(prop) {
+    return { __html: prop };
+  }
+  function MyComponent({ prop }) {
+    return <div dangerouslySetInnerHTML={ createMarkup(prop) } />;
+  }
   return (
     <div>
       <h1>{fish['Species Name']}</h1>
       <p> Cholesterol: {fish['Cholesterol']}</p>
+      <div>Biology: {<MyComponent prop={fish.Biology}/>}</div>
+      <div>Habitat: {<MyComponent prop={fish.Habitat}/>}</div>
+      
     </div>
   );
 }

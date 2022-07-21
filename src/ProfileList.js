6c063929bda53ad55e301308d35395b66db6247d
Pedro <pedro.chn01@gmail.com>
2022-07-21 09:40:03 -0700

    profiles working

--- src/ProfileList.js
+++ src/ProfileList.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function ProfileList({ fishes }) {
-
   return (
     <div> 
       {
-        fishes.map((fish, i) => 
+        fishes && fishes.map((fish, i) => 
           <Link className='fish-div' to={`/fish/${fish['Species Name']}`} key={fish + i}>
             <p>{fish['Species Name']}</p>
             <img className="fish-pic" src={fish['Species Illustration Photo'].src}/>

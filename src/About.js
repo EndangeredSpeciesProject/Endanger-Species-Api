f759b645b075557b14f487af6c35e3c5f67546b9
austinbhan <austinbhan@outlook.com>
2022-07-21 14:20:23 -0700

    Austin

--- src/About.js
+++ src/About.js
@@ -4,27 +4,24 @@ export default function About() {
   return (
     <div className="bios-and-links">
       <div className="bio-box">
-        <img src="/images/pedro.jpeg" />
+        <img className="profile-pic" src="/images/pedro.jpeg" />
         <h3>Pedro Chavez-Nolasco</h3>
-        <h4>One sentence text for Pedro</h4>
         <div className="icons">
           <a href="https://www.linkedin.com/in/pedro-chaveznolasco/" target="/blank"><img src="/images/LinkedIn-icon.png" className="linkedin-icon" /></a>
           <a href="https://github.com/PCN23" target="/blank"><img src="/images/GitHub-icon.png" className="github-icon" /></a>
         </div>
       </div>
       <div className="bio-box">
-        <img src="/images/austin/.jpeg" />
+        <img className="profile-pic" src="/images/austin.jpeg" />
         <h3>Austin Han</h3>
-        <h4>One sentence text for Austin</h4>
         <div className="icons">
           <a href="https://www.linkedin.com/in/austin-han-740a69157/" target="/blank"><img src="/images/LinkedIn-icon.png" className="linkedin-icon" /></a>
           <a href="https://github.com/austinbhan" target="/blank"><img src="/images/GitHub-icon.png" className="github-icon" /></a>
         </div>
       </div>
       <div className="bio-box">
-        <img src="/images/amanda/.jpeg" />
+        <img className="profile-pic" src="/images/amanda.jpeg" />
         <h3>Amanda Hecht</h3>
-        <h4>One sentence text for Amanda</h4>
         <div className="icons">
           <a href="https://www.linkedin.com/in/amanda-hecht/" target="/blank"><img src="/images/LinkedIn-icon.png" className="linkedin-icon" /></a>
           <a href="https://github.com/amanda-hecht89" target="/blank"><img src="/images/GitHub-icon.png" className="github-icon" /></a>
@@ -32,9 +29,8 @@ export default function About() {
       </div>
 
       <div className="bio-box">
-        <img src="/images/chad/.jpeg" />
+        <img className="profile-pic" src="/images/chad.jpeg" />
         <h3>Chad Stabler</h3>
-        <h4>One sentence text for Chad</h4>
         <div className="icons">
           <a href="https://www.linkedin.com/in/chad-stabler/" target="/blank"><img src="/images/LinkedIn-icon.png" className="linkedin-icon" /></a>
           <a href="https://github.com/Chad-Stabler" target="/blank"><img src="/images/GitHub-icon.png" className="github-icon" /></a>
